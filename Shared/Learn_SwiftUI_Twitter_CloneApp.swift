016f8454b810ff2cf7f3359c9cd7d1cf3c4f7050
ThienVuLe <vule@gumiviet.com>
2022-07-05 10:18:10 +0700

    Initial Commit

---
+++ Shared/Learn_SwiftUI_Twitter_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learn_SwiftUI_Twitter_CloneApp.swift
+//  Shared
+//
+//  Created by Thien Vu Le on Jul/5/22.
+//
+
+import SwiftUI
+
+@main
+struct Learn_SwiftUI_Twitter_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
